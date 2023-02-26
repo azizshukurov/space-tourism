a662a85253100ac4f059af1e6f03ebc229367fca
azizbekshukurov <azizbekshukurov11@gmail.com>
2023-02-26 12:06:47 +0500

    space tourism exam

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
-let tabLink = document.querySelectorAll('.tab__link')
-let tabLinkParent = document.querySelector('.tabs')
-let tabContent = document.querySelectorAll('.planet__content') 
+const tabLink = document.querySelectorAll('.tab__link')
+const tabLinkParent = document.querySelector('.tabs')
+const tabContent = document.querySelectorAll('.planet__content')
 
-addActiveClass()
+addActiveClass(0)
 
 function removeActiveClass(){
-	tabContent.forEach(item => {
-		item.classList.remove('active')
-	})
+    tabContent.forEach(item => {
+        item.classList.remove('active')
+    })
 }
 
 function addActiveClass(index = 0){
-	tabContent[index].classList.add('active')
+    tabContent[index].classList.add('active')
 }
 
 tabLinkParent.addEventListener('click', (e) => {
-	tabLink.forEach((link, i) => {
-		if(e.target === link){
-			removeActiveClass()
-			addActiveClass(i)
-		}
-	})
+    tabLink.forEach((link, i) => {
+        if(e.target === link){
+            removeActiveClass()
+            addActiveClass(i)
+        }
+    })
 })
